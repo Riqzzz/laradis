c913d3533dd7d0f2405bd10da4e02b9e37440105
Riqzzz <tariqaziz2801@gmail.com>
2025-01-18 20:08:25 +0700

    Home Page CTA dan Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -173,6 +173,50 @@
             </div>
           </div>
         </section>
+        <section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+          <h2>Ready to Contribute?</h2>
+          <p class="mb-4" {{-- marbot 4=32px --}}>Want to make a big impact?</p>
+          <div class="text-center">
+            <a href="#" class="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+            <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Discussion</a>
+          </div>
+        </section>
+        <footer class="py-80px">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 col-lg-6 mb-5 mbb-lg-0">
+                <img src="{{ url('assets/images/Logo-footer.png')}}" class="h-32px mb-4" alt="Logo Footer">
+                <p class="text-white">Empowering the Laravel Community <br>to connect. share and learn</p>
+              </div>
+              <div class="col-12 col-lg-6 me-auto">
+                <div class="d-flex flex-column flex-lg-row justify-content-end">
+                  <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                    <p class="fw-bold fs-5 text-white text-nowrap">Contact Us</p>
+                    <ul class="list-unstyled">
+                      <li class="text-white">
+                        <a href="mailto:mr.towick@gmail.com">mr.towick@gmail.com</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="d-flex flex-column">
+                    <p class="fw-bold fs-5 text-white">Links</p>
+                    <ul class="list-unstyled">
+                      <li class="text-white mb-2">
+                        <a href="#">Home</a>
+                      </li>
+                      <li class="text-white mb-2">
+                        <a href="#">Discussions</a>
+                      </li>
+                      <li class="text-white mb-2">
+                        <a href="#">About Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
 </html>
